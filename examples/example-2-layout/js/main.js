fb02a17e2cfcbdd6322ce3b1204ebee1b391c537
Garry Ing <ging@vmware.com>
2020-07-09 17:52:19 -0400

    convert to jquery

--- examples/example-2-layout/js/main.js
+++ examples/example-2-layout/js/main.js
@@ -5,29 +5,18 @@
 
 console.log('Welcome to Developer Tools. JavaScript loaded from main.js!');
 
-// Two variables selecting two buttons in index.html
-// Both are looking elements with their respective classes
-// The name of the variables are arbitrary, but should be consistent
-// AVOID: Using spaces for variables
+// Using the dot notation (similar to css), a click listener is added to the button that will visually change the page
 
-var myButtonChange = document.querySelector('.button-change');
-var myButtonReset = document.querySelector('.button-reset');
-
-// Using the dot notation, a click listener is added to the button that will visually change the page
-// The function after the click contains a single argument that could be used
-// Using the argument is optional, but helpful for more complex events
-
-myButtonChange.addEventListener('click', function( event ) {
-  // Using the querySelector, we are changing the <body> tag to use inline CSS
-  // The CSS is written as declarations since the element is implied
-
-  document.querySelector('body').style.cssText = 'background: #ff0; color: #00f';
-});
+$('.button-change').on('click', function(){
+  // CSS will be added to <body>
+  $('body').css('background', '#ff0')
+  // More CSS to add to the <body>
+  $('body').css('color', '#00f')
+})
 
 // A click listener is added to the button that will reset the page's style
 
-myButtonReset.addEventListener('click', function( event ) {
-  // The existing Style attribute in <body> will be replaced with this value
-
-  document.querySelector('body').style.cssText = 'background: #fff';
-});
+$('.button-reset').on('click', function(){
+  // Change the body background back to white
+  $('body').css('background', '#fff')
+})

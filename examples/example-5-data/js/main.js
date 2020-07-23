0074548302fd762db54bcdfa25453dce46282809
Garry Ing <garry.ing@gmail.com>
2020-07-22 21:31:27 -0400

    fixes

--- examples/example-5-data/js/main.js
+++ examples/example-5-data/js/main.js
@@ -3,16 +3,16 @@
 // Console.log outputs a message to your inspect tool
 // If you see this in your Console, there's a good chance that this file has been linked correctly
 
-console.log('Welcome to Developer Tools. JavaScript loaded from main.js!');
+console.log('Welcome to Developer Tools. JavaScript loaded from main.js!')
 
 $.ajax({
   // Retrieve data from News API through a special URL (Endpoint)
-  url: "https://gnews.io/api/v3/search?q=toronto&image=required&token=d94bc2a40e081c5e0348456186c1d06b"
-}).done(function( data ) {
+  url: 'https://gnews.io/api/v3/search?q=toronto&image=required&token=d94bc2a40e081c5e0348456186c1d06b'
+}).done(function (data) {
   // After we have retrieved the data, display the data in the browser's console
-  console.log(data);
+  console.log(data)
   // Take each article from the source and perform a function to each
-  data.articles.map(function(article){
+  data.articles.map(function (article) {
     // Display all the content in a div with the class of article.
     //
     // Append each article from the API in the following format where instances of ${article.propery}
@@ -25,6 +25,6 @@ $.ajax({
         <p>${article.description}</p>
         <a href="${article.url}" class="readmore-link">Read More</a>
       </div>
-    `);
-  });
-});
+    `)
+  })
+})
